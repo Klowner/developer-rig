34b4316ae5b6270d34992f1f5c87f92448c037d7
Josh Madewell <joshmade@twitch.tv>
2018-08-30 11:47:15 -0500

    Allow feature flag spoofing during view creation

--- src/extension-view-dialog/component.tsx
+++ src/extension-view-dialog/component.tsx
@@ -22,6 +22,7 @@ export type ExtensionViewDialogProps = {
 export type ExtensionViewDialogState = {
   extensionViewType: ExtensionAnchor | ExtensionMode | ExtensionPlatform | ExtensionViewType;
   frameSize: string;
+  isChatEnabled: boolean;
   viewerType: string;
   x: number;
   y: number;
@@ -30,11 +31,12 @@ export type ExtensionViewDialogState = {
   identityOption: string;
   orientation: string;
   opaqueId: string;
-  [key: string]: number | string;
+  [key: string]: number | string | boolean;
 };
 
-const InitialState = {
+const InitialState: ExtensionViewDialogState = {
   extensionViewType: DefaultExtensionType,
+  isChatEnabled: false,
   frameSize: DefaultOverlaySize,
   viewerType: DefaultViewerType,
   x: 0,
@@ -141,8 +143,12 @@ export class ExtensionViewDialog extends React.Component<ExtensionViewDialogProp
     this.props.closeHandler();
   }
 
-  save = () => {
+  private save = () => {
     this.props.saveHandler(this.state);
+  }
+
+  private toggleIsChatEnabled = () => {
+    this.setState({ isChatEnabled: !this.state.isChatEnabled });
   }
 
   public render() {
@@ -281,7 +287,7 @@ export class ExtensionViewDialog extends React.Component<ExtensionViewDialogProp
               </div>
             </div>
           </div>
-          {this.state.extensionViewType !== ExtensionMode.Config && this.state.extensionViewType !== ExtensionMode.Dashboard &&
+          {this.state.extensionViewType !== ExtensionMode.Config && this.state.extensionViewType !== ExtensionMode.Dashboard && (
             <div className="dialog__viewer-container">
               <div className="type-title__type-container">
                 <div className="type-and-size-container__type-title">
@@ -299,7 +305,22 @@ export class ExtensionViewDialog extends React.Component<ExtensionViewDialogProp
                   </div>
                 </div>
               </div>
-            </div>}
+            </div>
+          )}
+          <div className="dialog__viewer-container">
+            <div className="type-title__type-container">
+              <div className="type-and-size-container__type-title">
+                Feature Flags
+              </div>
+              <div className='dialog__type-container'>
+                <div className="option-div">
+                  <label className="option-label">
+                    <input id="isChatEnabled" type="checkbox" onChange={this.toggleIsChatEnabled} checked={this.state.isChatEnabled} />isChatEnabled
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
           <hr className="dialog__divider" />
           <div className="dialog_bottom-bar">
             <div className="bottom-bar__save" onClick={this.save}> Save </div>
